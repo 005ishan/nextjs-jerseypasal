d0caf7a8fc154fbc843dcb66bb4caef31ebd87e8
0050505ishan <isan91798@gmail.com>
2026-02-02 12:37:31 +0545

    authcontext

---
+++ context/AuthContext.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import { createContext, useContext, useEffect, useState } from "react";
+
+type User = {
+  _id: string;
+  email: string;
+  role: "admin" | "user";
+  token: string;
+};
+
+type AuthContextType = {
+  user: User | null;
+  login: (data: User) => void;
+  logout: () => void;
+};
+
+const AuthContext = createContext<AuthContextType | null>(null);
+
+export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
+  const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    const stored = localStorage.getItem("user");
+    if (stored) setUser(JSON.parse(stored));
+  }, []);
+
+  const login = (data: User) => {
+    localStorage.setItem("user", JSON.stringify(data));
+    setUser(data);
+  };
+
+  const logout = () => {
+    localStorage.removeItem("user");
+    setUser(null);
+  };
+
+  return (
+    <AuthContext.Provider value={{ user, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => useContext(AuthContext)!;
